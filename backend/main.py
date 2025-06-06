52d83d7e8a0a8264967a8cc6e7ca93aaf8709f13
Gokul <gokulchandan24@gmail.com>
2025-06-07 02:37:07 +0530

    model images created

---
+++ backend/main.py
@@ -0,0 +1,31 @@
+import sys
+
+import modal
+
+evo2_image = (
+    modal.Image.from_registry(
+        "nvidia/cuda:12.4.0-devel-ubuntu22.04", add_python="3.12"
+    )
+    .apt_install(
+        ["build-essential", "cmake", "ninja-build",
+            "libcudnn8", "libcudnn8-dev", "git", "gcc", "g++"]
+    )
+    .env({
+        "CC": "/usr/bin/gcc",
+        "CXX": "/usr/bin/g++",
+    })
+    .run_commands("git clone --recurse-submodules https://github.com/ArcInstitute/evo2.git && cd evo2 && pip install .")
+    .run_commands("pip uninstall -y transformer-engine transformer_engine")
+    .run_commands("pip install 'transformer_engine[pytorch]==1.13' --no-build-isolation")
+    .pip_install_from_requirements("requirements.txt")
+)
+
+app = modal.App("variant-analysis-evo2", image=evo2_image)
+
+@app.function(gpu="H100")
+def test():
+    print("TESTING")
+
+@app.local_entrypoint()
+def main():
+    test.remote()
